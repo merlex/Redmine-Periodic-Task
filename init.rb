c42c88f6b65f06f4f1f0396b3775d2783c40ddcd
Tony Marschall <tnix@dev.paupau.de>
2011-11-19 22:48:53 +0100

    added per project option, fixed permission

--- init.rb
+++ init.rb
@@ -4,8 +4,13 @@ Redmine::Plugin.register :redmine_periodictask do
   name 'Redmine Periodictask plugin'
   author 'Tanguy de Courson'
   description 'This is a plugin for Redmine that will allow you to schedule a task to be assigned on a schedule'
-  version '1.0.0'
+  version '1.0.1'
 
-  permission :periodictask, {:periodictask => [:index, :edit]}, :public => true
+  project_module :redmine_periodictask do
+
+    permission :view_periodictask, :periodictask => :index
+    permission :edit_periodictask, :periodictask => :edit
+  end
+  
   menu :project_menu, :periodictask, { :controller => 'periodictask', :action => 'index' }, :caption => :title_periodic_task, :after => :settings, :param => :project_id
 end
