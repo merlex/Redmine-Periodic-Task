241c72946df95649f8e1e16be3860f897003d1a0
MerLex <merlex85@gmail.com>
2012-05-21 14:39:15 +0400

    fix "next run date"

--- lib/scheduled_tasks_checker.rb
+++ lib/scheduled_tasks_checker.rb
@@ -1,6 +1,6 @@
 class ScheduledTasksChecker
   def self.checktasks!
-     Periodictask.all(:conditions=> ["next_run_date <= ? ", Time.now.to_date]).each do |task| 
+     Periodictask.all(:conditions=> ["next_run_date <= ? ", Time.now.to_date.strftime("%Y-%m-%d 23:59:59")]).each do |task|
        print "assigning #{task.subject}"
          issue = Issue.new(:project_id=>task.project_id,  :tracker_id=>task.tracker_id, :assigned_to_id=>task.assigned_to_id, :author_id=>task.author_id, :subject=>task.subject, :description=>task.description, :priority_id=>task.priority_id);
          issue.save         
